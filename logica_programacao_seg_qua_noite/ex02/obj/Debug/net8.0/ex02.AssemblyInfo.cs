f84731791b7e93cf655062c6b5fd822067f944f4
JULIANA PEREIRA NOGUEIRA <juliana.pnogueira1@senacsp.edu.br>
2025-01-13 22:04:36 -0300

    ex.04

--- logica_programacao_seg_qua_noite/ex02/obj/Debug/net8.0/ex02.AssemblyInfo.cs
+++ logica_programacao_seg_qua_noite/ex02/obj/Debug/net8.0/ex02.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03114d839132042b0c35699ff55b4ffdc54007a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ logica_programacao_seg_qua_noite/ex04/Program.cs
@@ -0,0 +1,35 @@
+﻿// tipos de dados
+
+string nome = "Juliana ";
+Console.WriteLine("o nome do usuario é: " + nome);
+
+int idade = 38;
+//console.writeline( "a idade do usuario é:" + idade+
+Console.WriteLine($"a idade do usuario é: {idade} anos");
+
+float carteira = 2.63f;
+
+char classificacao = 'A';
+Console.WriteLine($"o cliente é do tipo {classificacao}");
+
+bool dinheironaconta = true;
+Console.WriteLine(dinheironaconta);
+
+double limitecredito = 2000.0;
+Console.WriteLine("seu limite de credito é: " + limitecredito);
+
+
+string sobrenome = "silva";
+
+string nomecompleto = nome + sobrenome;
+Console.WriteLine(nomecompleto);
+
+double valorconta = 53.73;
+Console.WriteLine($"o valor na conta é: {valorconta}");
+
+double limitetotal = valorconta + limitecredito;
+Console.WriteLine($"o saldo + limite é {limitetotal}");
+
+
+
+
